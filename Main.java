5d6b913e4263224398b453cdff8116d1692051e5
Prasad-kancha <prasad.kancha@snaddevelopers.com>
2026-06-04 11:42:43 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,36 @@
-package org.example;
+package sam.example;
+
+
+
+
+import org.hibernate.*;
+import org.hibernate.cfg.Configuration;
 
 public class Main {
-
+    public static void main(String[] args) {
+    	
+	      
+	      Configuration config=new Configuration();
+	     // config.addAnnotatedClass(sam.example.Employee.class);
+	      config.configure("hibernate.cfg.xml");
+	      
+	      SessionFactory factory= config.buildSessionFactory();
+	      
+	      Session session =factory.openSession();
+	      
+	      Transaction transaction=session.beginTransaction();
+	      Employee e=new Employee(104,"ravi","ph");
+	    	
+//	      e.setAid(102);
+//	      e.setAname("prasa");
+//	      e.setTech("python");
+	      session.persist(e);
+	      transaction.commit();
+	      
+	      
+	      
+	      
+    }
+    
+    
 }
